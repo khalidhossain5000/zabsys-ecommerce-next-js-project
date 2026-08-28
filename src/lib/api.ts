0c03d12620263c56a0b2e14373cdd76c2065a9ac
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-08-28 12:23:11 +0600

    all products card added in products component

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,13 +1,13 @@
-export const getAllProductsData=async()=>{
-    const res=await fetch("https://api.restful-api.dev/objects",{
-        cache:"force-cache",
-        next:{
-            revalidate:60*60*1,
-            tags:["all-products"]
+export const getAllProductsData = async () => {
+    const res = await fetch("https://api.restful-api.dev/objects", {
+        cache: "force-cache",
+        next: {
+            revalidate: 60 * 60 * 1,
+            tags: ["all-products"]
         }
     })
 
-    const result=await res.json()
+    const result = await res.json()
 
     return result
 }
