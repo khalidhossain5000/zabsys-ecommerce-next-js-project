857c42530bf91a9066343db776c0da50dde3dee1
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-08-28 11:37:29 +0600

    title reusbla ecomponent added

---
+++ src/components/shared/Title/Title.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ITitleProps {
+  children: React.ReactNode;
+  className?: string;
+  subTitle?: string;
+  subTitleClass?: string;
+}
+
+const Title = ({
+  children,
+  className,
+  subTitle,
+  subTitleClass,
+}: ITitleProps) => {
+  return (
+    <div className="container relative z-600 mx-auto px-4">
+      <h2
+        className={twMerge(
+          "font-manrope text-3xl font-semibold leading-tight tracking-tight sm:text-4xl lg:text-5xl xl:text-6xl",
+          className
+        )}
+      >
+        <span className="bg-linear-to-r from-accent via-teal-900 to-teal-400 bg-clip-text text-transparent dark:to-text-primary">
+          {children}
+        </span>
+      </h2>
+
+      {subTitle && (
+        <p
+          className={twMerge(
+            "text-sm leading-relaxed text-text-secondary sm:text-xl lg:mx-0",
+            subTitleClass
+          )}
+        >
+          {subTitle}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Title;
