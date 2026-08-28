97e8fa6cf2794f9344dd102d62d99659e859b8ad
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-08-28 14:34:57 +0600

    product detail data fetch in page and added

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,8 +1,17 @@
+import ProductDetails from "@/components/ProductDetailsPage/ProductDetails";
+import { getProductDetails } from "@/lib/api";
 
-const ProductsDetailsPage = () => {
+interface IParamsProps {
+  params: Promise<{ id: string }>;
+}
+const ProductsDetailsPage = async ({params}:IParamsProps) => {
+    const {id}=await params
+    const productDetails=await getProductDetails(id)
+
+    console.log(productDetails,'this is the details')
     return (
         <div>
-            
+            <ProductDetails product={productDetails}/>
         </div>
     );
 };
---
+++ src/components/ProductDetailsPage/ProductDetails.tsx
@@ -0,0 +1,14 @@
+import { IProduct } from "@/Types/types";
+
+interface IProductDetailsProps{
+    product:IProduct
+}
+const ProductDetails = ({product}:IProductDetailsProps) => {
+    return (
+        <section>
+            
+        </section>
+    );
+};
+
+export default ProductDetails;
