931af8c50bb9457785e8fb704f631d8e1b991032
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-08-28 14:22:35 +0600

    get product details api added

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -14,3 +14,16 @@ export const getAllProductsData = async (): Promise<IProduct[]> => {
     return result
 }
 
+export const getProductDetails=async(id:string):Promise<IProduct>=>{
+       const res = await fetch(`https://api.restful-api.dev/objects/${id}`, {
+        cache: "force-cache",
+        next: {
+            revalidate: 60 * 60 * 1,
+            tags: ["all-products"]
+        }
+    })
+
+    const result = await res.json()
+
+    return result
+}
