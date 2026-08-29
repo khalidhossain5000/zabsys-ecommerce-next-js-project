0b60d780b00e20a42b3e5a356fed7a5810662847
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-08-29 11:43:52 +0600

    style:all product page title is added

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,15 @@
 import Products from "@/components/AllProducts/Products";
 import AllProductsSkeleton from "@/components/shared/Loading/Products/AllProductsSkleton";
+import Title from "@/components/shared/Title/Title";
 import { Suspense } from "react";
 
 const AllProductsPage = () => {
   return (
     <div>
+      <Title className="text-center md:text-center pt-12">
+        All Products Page Showing All Available Products
+      </Title>
+
       <Suspense fallback={<AllProductsSkeleton />}>
         {" "}
         <Products />
